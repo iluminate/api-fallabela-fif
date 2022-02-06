e2455ed407b98c0fb241306739e6b893992a2996
Kevyn Estrada <kevyn.estrada.contractor@bbva.com>
2022-02-05 21:44:06 -0500

    add test for layer repository

--- test/repositories/beer_test.go
+++ test/repositories/beer_test.go
@@ -4,20 +4,32 @@ import (
 	"api-fallabela-fif/application/entities"
 	"api-fallabela-fif/application/repositories"
 	"api-fallabela-fif/helpers/database"
+	"api-fallabela-fif/test/mocks"
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"reflect"
 	"testing"
 )
 
 func TestNewBeerRepository(t *testing.T) {
+
+	mockMongodbHelper := new(mocks.MockMongodbHelper)
+
 	type args struct {
-		database *database.MongodbHelper
+		database database.IMongodbHelper
 	}
+
 	tests := []struct {
 		name string
 		args args
-		want *repositories.beerRepository
+		want repositories.IBeerRepository
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{database: mockMongodbHelper},
+			want: repositories.NewBeerRepository(mockMongodbHelper)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -29,24 +41,46 @@ func TestNewBeerRepository(t *testing.T) {
 }
 
 func Test_beerRepository_Create(t *testing.T) {
+
+	sampleBeerSuccess := entities.Beer{
+		Id:       1,
+		Name:     "Pilsen",
+		Brewery:  "Backus",
+		Country:  "Peru",
+		Price:    5,
+		Currency: "PEN",
+	}
+
+	mockMongoCollection := new(mocks.MockMongoCollection)
+	mockMongoCollection.On("InsertOne", context.Background(), &sampleBeerSuccess).Return(
+		&mongo.InsertOneResult{InsertedID: 1}, nil)
+	mockMongoCollection.On("InsertOne", context.Background(), &entities.Beer{}).Return(
+		&mongo.InsertOneResult{InsertedID: 1}, errors.New("duplicate key error"))
+
+	mockMongodbHelper := new(mocks.MockMongodbHelper)
+	mockMongodbHelper.On("Open").Return(nil)
+	mockMongodbHelper.On("Collection", "beers").Return(mockMongoCollection)
+
 	type fields struct {
-		database *database.MongodbHelper
+		database database.IMongodbHelper
 	}
 	type args struct {
 		beer *entities.Beer
 	}
+
 	tests := []struct {
 		name    string
 		fields  fields
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "success", fields: fields{database: mockMongodbHelper}, args: args{beer: &sampleBeerSuccess}, wantErr: false},
+		{name: "failure", fields: fields{database: mockMongodbHelper}, args: args{beer: &entities.Beer{}}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			repo := repositories.beerRepository{
-				database: tt.fields.database,
+			repo := repositories.BeerRepository{
+				Database: tt.fields.database,
 			}
 			if err := repo.Create(tt.args.beer); (err != nil) != tt.wantErr {
 				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
@@ -56,21 +90,33 @@ func Test_beerRepository_Create(t *testing.T) {
 }
 
 func Test_beerRepository_FindAll(t *testing.T) {
+
+	var sampleBeers []entities.Beer
+
+	mockMongoCollection := new(mocks.MockMongoCollection)
+	mockMongoCollection.On("Find", context.Background(), bson.D{}).Return(
+		&mongo.Cursor{}, errors.New("timeout error"))
+
+	mockMongodbHelper := new(mocks.MockMongodbHelper)
+	mockMongodbHelper.On("Open").Return(nil)
+	mockMongodbHelper.On("Collection", "beers").Return(mockMongoCollection)
+
 	type fields struct {
-		database *database.MongodbHelper
+		database database.IMongodbHelper
 	}
+
 	tests := []struct {
 		name    string
 		fields  fields
 		want    *[]entities.Beer
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "failure", fields: fields{database: mockMongodbHelper}, want: &sampleBeers, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			repo := repositories.beerRepository{
-				database: tt.fields.database,
+			repo := repositories.BeerRepository{
+				Database: tt.fields.database,
 			}
 			got, err := repo.FindAll()
 			if (err != nil) != tt.wantErr {
@@ -85,8 +131,17 @@ func Test_beerRepository_FindAll(t *testing.T) {
 }
 
 func Test_beerRepository_FindById(t *testing.T) {
+
+	mockMongoCollection := new(mocks.MockMongoCollection)
+	mockMongoCollection.On("FindOne", context.Background(), bson.D{{"_id", int64(1)}}).Return(
+		&mongo.SingleResult{})
+
+	mockMongodbHelper := new(mocks.MockMongodbHelper)
+	mockMongodbHelper.On("Open").Return(nil)
+	mockMongodbHelper.On("Collection", "beers").Return(mockMongoCollection)
+
 	type fields struct {
-		database *database.MongodbHelper
+		database database.IMongodbHelper
 	}
 	type args struct {
 		id int64
@@ -98,12 +153,12 @@ func Test_beerRepository_FindById(t *testing.T) {
 		want    *entities.Beer
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "success", fields: fields{database: mockMongodbHelper}, args: args{id: 1}, want: nil, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			repo := repositories.beerRepository{
-				database: tt.fields.database,
+			repo := repositories.BeerRepository{
+				Database: tt.fields.database,
 			}
 			got, err := repo.FindById(tt.args.id)
 			if (err != nil) != tt.wantErr {
